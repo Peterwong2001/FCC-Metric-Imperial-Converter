1f594c403a88b40f0a55ecfa80d9741ab55f54f3
Glitch (material-mesquite-taleggio) <none>
2024-03-03 13:50:14 +0000

    💡😽 Checkpoint ./routes/api.js:65325151/664

--- routes/api.js
+++ routes/api.js
@@ -6,5 +6,22 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
-
+  app.route('/api/convert')
+  .get(function(req, res) {
+    let input = req.query.input;
+    let initNum = convertHandler.getNum(input);
+    let initUnit = convertHandler.getUnit(input);
+    let returnNum = convertHandler.convert(initNum, initUnit);
+    let returnUnit = convertHandler.convert(initUnit);
+    let toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+    
+    let resObj = {};
+    resObj['initNum'] = initNum
+    resObj['initUnit'] = initUnit
+    resObj['returnNum'] = returnNum
+    resObj['returnUnit'] = returnUnit
+    resObj['string'] = toString
+    
+    res.json(resObj)
+  })
 };
