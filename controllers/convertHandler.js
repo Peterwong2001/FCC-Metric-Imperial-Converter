4f73dd5f4002a6003163b8bf0b256365097721ca
Glitch (material-mesquite-taleggio) <none>
2024-03-04 18:56:30 +0000

    🌛💗 Checkpoint ./controllers/convertHandler.js:65325151/7

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -17,7 +17,7 @@ function ConvertHandler() {
       
       let values = result.toString().split('/')
       if (values.length != 2) {
-    	return 'invalid number'   
+    	  return 'invalid number'   
       } else {
           values[0] = parseFloat(values[0])
           values[1] = parseFloat(values[1])
@@ -30,6 +30,7 @@ function ConvertHandler() {
     }
     
     return result;
+    
   };
   
   this.getUnit = function(input) {
